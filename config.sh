3048ea0456acf24786861d7d935b58208ec7cecc
Haris Gušić <harisgusic.dev@gmail.com>
2021-05-27 02:32:13 +0200

    Add a reasonable feature set
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,15 @@
-
-DELAY=0.02
-DELAY_SEP=0.08
-DELAY_PROMPT=0.6
-COLOR_CMDLINE='1;33'
-COLOR_WARNING='1;91'
+configure() {
+    DELAY=0.02
+    DELAY_SEP=0.08
+    DELAY_PROMPT=0.6
+    COLOR_CMDLINE='1;33'
+    COLOR_WARNING='1;91'
+    COLOR_MESSAGE='1;32'
+}
 
 # Command line prompt
 prompt() {
     echo -n '$ '
 }
+
+# vim: filetype=bash
