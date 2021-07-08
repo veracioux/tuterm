2472b3ff50f02aad66a7c9fe5d1d7c9fbba1f9f3
Haris Gušić <harisgusic.dev@gmail.com>
2021-07-08 23:40:22 +0200

    A lot of tweaks
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,9 @@
-configure() {
-    DELAY=0.02
-    DELAY_SEP=0.08
-    DELAY_PROMPT=0.6
-    COLOR_CMDLINE='1;33'
-    COLOR_WARNING='1;91'
-    COLOR_MESSAGE='1;32'
-}
+DELAY=0.02
+DELAY_SEP=0.08
+DELAY_PROMPT=0.6
+COLOR_CMDLINE='1;33'
+COLOR_WARNING='1;91'
+COLOR_MESSAGE='1;32'
 
 # Command line prompt
 prompt() {
